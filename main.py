81a58cce58c9462c50100906e058682e1af3c9ba
orbirpinar <orbirpinar@gmail.com>
2022-08-16 00:01:13 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+import csv
+import json
+from os.path import exists
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/teachers", methods=['POST'])
+def get_teachers():
+    school_code = request.json["school_code"]
+    district_code = request.json['district_code']
+    city_code = request.json["city_code"]
+    school_website = request.json['school_website']
+    fileName = f"{school_code}.json"
+    if (exists(fileName)):
+        data = get_from_cache(fileName)
+        return jsonify(
+            {'data': data}
+        )
+
+    url = f"{school_website}/{city_code}/{district_code}/{school_code}/teskilat_semasi.html"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    all_teacher_link_tag = soup.select(".sitemap li a")
+    teachers = []
+    for i in all_teacher_link_tag:
+        name = i.find(text=True)
+        nameDict = split_to_firstname_and_lastname(name)
+        firstname = nameDict['firstname']
+        lastname = nameDict['lastname']
+        department = i.get("title", "No department")
+        teacherObject = {'firstname': firstname, 'lastname': lastname, 'department': department}
+        teachers.append(teacherObject)
+    write_json(f"{school_code}.json", teachers)
+    return jsonify(
+        {'data': teachers}
+    )
+
+
+def split_to_firstname_and_lastname(name):
+    nameList = name.split(" ")
+    lastname = ""
+    firstname = ""
+    for i in nameList:
+        if i[1].isupper():
+            lastname += i + " "
+        else:
+            firstname += i + " "
+    return {"firstname": firstname.strip(), "lastname": lastname.strip()}
+
+
+@app.route("/api/v1/students")
+def get_students():
+    filename = 'students.json'
+    if exists(filename):
+        data = get_from_cache(filename)
+        return jsonify(
+            {"data": data}
+        )
+    data = csv_to_json()
+    return jsonify(
+        {"data": data}
+    )
+
+
+def csv_to_json():
+    json_list = []
+    with open("students.csv", encoding="utf-8") as file:
+        csv_reader = csv.DictReader(file)
+
+        for row in csv_reader:
+            _class = row['class'].split("/")
+            row["lastname"] = row['name'].split().pop(-1)
+            row["firstname"] = row['name'].rsplit(" ", 1)[0]
+            level = _class[0]
+            branch = _class[1]
+            row['level'] = level.replace(". Sınıf", "").strip()
+            row['branch'] = branch.replace("Şubesi", "").strip()
+            json_list.append(row)
+    # convert python jsonArray to JSON String and write to file
+    write_json("students.json", json_list)
+    return json_list
+
+
+def write_json(file_name, content):
+    with open(file_name, 'w+', encoding='utf-8') as jsonf:
+        jsonString = json.dumps(content, indent=4)
+        print(jsonString)
+        jsonf.write(jsonString)
+
+
+def get_from_cache(fileName):
+    with open(fileName, encoding='utf-8') as file:
+        return json.load(file)
